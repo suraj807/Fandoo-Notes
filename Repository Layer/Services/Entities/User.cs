ee873e38d52d2b5db328a57b387e3bda19b68fb7
suraj807 <surajdev805@gmail.com>
2022-06-26 14:44:18 +0530

    add user

---
+++ Repository Layer/Services/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Repository_Layer.Services.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ Repository Layer/Services/FundooContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository_Layer.Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository_Layer.Services
+{
+    public class FundooContext : DbContext
+    {
+        public FundooContext(DbContextOptions<FundooContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
